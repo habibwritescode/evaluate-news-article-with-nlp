b90304f69cadc1303d99e11fb7ca5407bf542681
Habib Afolabi <habibwritescode@gmail.com>
2020-04-14 21:10:48 +0100

    Add sass loader and style loader

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,6 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+import './styles/resets.scss'
 
 console.log(checkForName);
 
--- webpack.dev.js
+++ webpack.dev.js
@@ -10,10 +10,15 @@ module.exports = {
     stats: 'verbose',
     module: {
         rules: [{
-            test: '/\.js$/',
-            exclude: /node_modules/,
-            loader: "babel-loader"
-        }]
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
     },
     plugins: [
         new HtmlWebPackPlugin({
