5719e13160fc5845b4fba77d2d062e3c161b2656
Habib Afolabi <habibwritescode@gmail.com>
2020-04-17 02:26:04 +0100

    Set up API

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,39 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv');
+dotenv.config();
 
-const app = express()
+var path = require('path');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mockAPIResponse = require('./mockAPI.js');
 
-app.use(express.static('dist'))
+var aylien = require("aylien_textapi");
+// set aylien API credentials
+// NOTICE that textapi is the name I used, but it is arbitrary.
+// You could call it aylienapi, nlp, or anything else, 
+//   just make sure to make that change universally!
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+});
 
-console.log(__dirname)
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static('dist'));
+
+console.log(__dirname);
 
 app.get('/', function(req, res) {
-    res.sendFile('dist/index.html')
-        // res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html');
+    // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
